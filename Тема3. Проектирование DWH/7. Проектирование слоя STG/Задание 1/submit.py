b332b1e89548ae1883311919548c638bc72c68e4
Pavel Kulagov <kulagov@yandex-team.ru>
2022-06-05 09:34:53 +0300

    rename folder and fix de05030701

---
+++ Тема3. Проектирование DWH/7. Проектирование слоя STG/Задание 1/submit.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+def submit(rlz_file, t_code):
+    full_lesson_path = os.path.dirname(os.path.abspath(__file__))
+    user_file = f'{full_lesson_path}/{rlz_file}'
+
+    with open(user_file, 'r') as u_file:
+        user_code = u_file.read()
+
+    r = requests.post(
+        'http://localhost:3002',
+        json={
+            "code": user_code,
+            "test": t_code
+            })
+
+    print(r.json()['stdout'])
+    print(r.json()['stderr'].replace('/app/__test.py',rlz_file))
+
+if __name__ == '__main__':
+    submit(
+        'realization.sql',
+        'de05030701'
+    )
